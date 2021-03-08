583e3402473052e84eadc7fa865c2243a1a74fd9
yarosshest <yarosshest@gmail.com>
2021-03-09 02:53:37 +0300

    assert add

--- Matric_compl.c
+++ Matric_compl.c
@@ -3,8 +3,11 @@
 #include "Matric_compl.h"
 #include "Matric .h"
 #include "compl_num.h"
+#include <assert.h>
+
 struct Matric init_compl_Matric(int element_size, int size,void* ar)
 {
+    assert(size > 0);
     struct Matric_compl *arT = (struct Matric_compl* ) calloc(1, sizeof (struct Matric_compl));
     arT->mas = ar;
 
@@ -22,53 +25,52 @@ struct Matric init_compl_Matric(int element_size, int size,void* ar)
 
 struct Matric mult_compl_Matric(struct Matric mat1, struct Matric mat2)
 {
-    if (mat1.size == mat2.size)
+    assert(mat1.size == mat2.size);
+    assert(mat1.element_size == mat2.element_size);
+
+    struct Matric_compl m1 = *( struct Matric_compl *)(mat1.arT);
+    struct Matric_compl m2 = *( struct Matric_compl *)(mat2.arT);
+
+    int size =mat1.size;
+
+    struct compl *mr = (struct compl*)calloc(1,size * size * sizeof(struct compl));
+    struct Matric ret = init_compl_Matric(sizeof(struct compl),size,mr);
+
+    for (int i = 0; i < size; i++)
     {
-        struct Matric_compl m1 = *( struct Matric_compl *)(mat1.arT);
-        struct Matric_compl m2 = *( struct Matric_compl *)(mat2.arT);
-
-        int size =mat1.size;
-
-        struct compl *mr = (struct compl*)calloc(1,size * size * sizeof(struct compl));
-        struct Matric ret = init_compl_Matric(sizeof(struct compl),size,mr);
-
-        for (int i = 0; i < size; i++)
+        for (int j = 0; j <size;j++)
         {
-            for (int j = 0; j <size;j++)
+            for(int z = 0; z < size; z++)
             {
-                for(int z = 0; z < size; z++)
-                {
-                    struct compl mult = mult_compl(m1.mas[i*size+z],m2.mas[z*size+j]);
-                    mr[i*size+j] = sum_compl(mr[i*size+j],mult);
-                }
+                struct compl mult = mult_compl(m1.mas[i*size+z],m2.mas[z*size+j]);
+                mr[i*size+j] = sum_compl(mr[i*size+j],mult);
             }
         }
-        return ret;
     }
+    return ret;
 }
 
 struct Matric sum_compl_Matric(struct Matric mat1, struct Matric mat2)
 {
-    if (mat1.size == mat2.size)
+    assert(mat1.size == mat2.size);
+    assert(mat1.element_size == mat2.element_size);
+    struct Matric_compl m1 = *( struct Matric_compl *)(mat1.arT);
+    struct Matric_compl m2 = *( struct Matric_compl *)(mat2.arT);
+
+    int size =mat1.size;
+
+    struct compl *mr = (struct compl*)calloc(1,size * size * sizeof(struct compl));
+    struct Matric ret = init_compl_Matric(sizeof(struct compl),size,mr);
+
+    for (int i = 0; i < size; i++)
     {
-        struct Matric_compl m1 = *( struct Matric_compl *)(mat1.arT);
-        struct Matric_compl m2 = *( struct Matric_compl *)(mat2.arT);
-
-        int size =mat1.size;
-
-        struct compl *mr = (struct compl*)calloc(1,size * size * sizeof(struct compl));
-        struct Matric ret = init_compl_Matric(sizeof(struct compl),size,mr);
-
-        for (int i = 0; i < size; i++)
+        for (int j = 0; j <size;j++)
         {
-            for (int j = 0; j <size;j++)
-            {
-                struct compl sum = sum_compl(m1.mas[i*size+j],m2.mas[i*size+j]);
-                mr[i*size+j] = sum_compl(mr[i*size+j],sum);
-            }
+            struct compl sum = sum_compl(m1.mas[i*size+j],m2.mas[i*size+j]);
+            mr[i*size+j] = sum_compl(mr[i*size+j],sum);
         }
-        return ret;
     }
+    return ret;
 }
 
 struct Matric trans_compl_Matric(struct Matric mat1)
--- Matric_int.c
+++ Matric_int.c
@@ -2,9 +2,11 @@
 #include "Matric_int.h"
 #include "Matric .h"
 #include "stdlib.h"
+#include <assert.h>
 
 struct Matric init_int_Matric(int element_size, int size,void* ar)
 {
+    assert(size > 0);
     struct Matric_int *arT = (struct Matric_int* ) calloc(1, sizeof (struct Matric_int));
     arT->mas = ar;
 
@@ -21,53 +23,53 @@ struct Matric init_int_Matric(int element_size, int size,void* ar)
 }
 struct Matric mult_int_Matric(struct Matric mat1, struct Matric mat2)
 {
-    if (mat1.size == mat2.size)
+    assert(mat1.size == mat2.size);
+    assert(mat1.element_size == mat2.element_size);
+
+    struct Matric_int m1 = *( struct Matric_int *)(mat1.arT);
+    struct Matric_int m2 = *( struct Matric_int *)(mat2.arT);
+
+    int size =mat1.size;
+
+    int *mr = (int*)calloc(1,size * size * sizeof(int));
+    struct Matric ret = init_int_Matric(sizeof(int),size,mr);
+
+    for (int i = 0; i < size; i++)
     {
-        struct Matric_int m1 = *( struct Matric_int *)(mat1.arT);
-        struct Matric_int m2 = *( struct Matric_int *)(mat2.arT);
-
-        int size =mat1.size;
-
-        int *mr = (int*)calloc(1,size * size * sizeof(int));
-        struct Matric ret = init_int_Matric(sizeof(int),size,mr);
-
-        for (int i = 0; i < size; i++)
+        for (int j = 0; j <size;j++)
         {
-            for (int j = 0; j <size;j++)
+            mr[i*size+j] = 0;
+            for(int z = 0; z < size; z++)
             {
-                mr[i*size+j] = 0;
-                for(int z = 0; z < size; z++)
-                {
-                    mr[i*size+j] = mr[i*size+j] + m1.mas[i*size+z]*m2.mas[z*size+j];
-                }
+                mr[i*size+j] = mr[i*size+j] + m1.mas[i*size+z]*m2.mas[z*size+j];
             }
         }
-        return ret;
     }
+    return ret;
 }
 
 struct Matric sum_int_Matric(struct Matric mat1, struct Matric mat2)
 {
-    if (mat1.size == mat2.size)
+    assert(mat1.size == mat2.size);
+    assert(mat1.element_size == mat2.element_size);
+
+    struct Matric_int m1 = *( struct Matric_int *)(mat1.arT);
+    struct Matric_int m2 = *( struct Matric_int *)(mat2.arT);
+
+    int size =mat1.size;
+
+    int *mr = (int*)calloc(1,size * size * sizeof(int));
+    struct Matric ret = init_int_Matric(sizeof(int),size,mr);
+
+    for (int i = 0; i < size; i++)
     {
-        struct Matric_int m1 = *( struct Matric_int *)(mat1.arT);
-        struct Matric_int m2 = *( struct Matric_int *)(mat2.arT);
-
-        int size =mat1.size;
-
-        int *mr = (int*)calloc(1,size * size * sizeof(int));
-        struct Matric ret = init_int_Matric(sizeof(int),size,mr);
-
-        for (int i = 0; i < size; i++)
+        for (int j = 0; j <size;j++)
         {
-            for (int j = 0; j <size;j++)
-            {
-                mr[i*size+j] = 0;
-                mr[i*size+j] = mr[i*size+j] + m1.mas[i*size+j]+m2.mas[i*size+j];
-            }
+            mr[i*size+j] = 0;
+            mr[i*size+j] = mr[i*size+j] + m1.mas[i*size+j]+m2.mas[i*size+j];
         }
-        return ret;
     }
+    return ret;
 }
 
 struct Matric trans_int_Matric(struct Matric mat1)
